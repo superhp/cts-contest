82878a4cc7216413094932e05c796d9feb835ad5
Aurimas Šimkus <saurimas@hotmail.com>
2017-09-08 10:48:10 +0300

    Balance and Solution logic classes added

---
+++ CtsContestWeb/CtsContestWeb.Logic/BalanceLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CtsContestWeb.Logic
+{
+    public class BalanceLogic
+    {
+        private int _amount;
+
+        public BalanceLogic(/* ... */)
+        {
+            _amount = 0;
+        }
+
+        public int Amount
+        {
+            get { return _amount; }
+        }
+
+        public bool IsBalanceEnough()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
