26fcb86edb60bd0ccb1a2ad35606874577351af7
Laurynas <laurynas.simkunas@cognizant.com>
2017-09-12 13:16:26 +0300

    Authentication test

--- CtsContestWeb/CtsContestWeb/Startup.cs
+++ CtsContestWeb/CtsContestWeb/Startup.cs
@@ -6,6 +6,13 @@ using Microsoft.Extensions.DependencyInjection;
 using CtsContestWeb.Db.DataAccess;
 using CtsContestWeb.Db;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Principal;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System;
+using Newtonsoft.Json.Linq;
 
 namespace CtsContestWeb
 {
@@ -57,6 +64,55 @@ namespace CtsContestWeb
                     serviceScope.ServiceProvider.GetService<ApplicationDbContext>().Database.Migrate();
                 }
             }
+
+            app.Use(async (context, next) =>
+            {
+                // Create a user on current thread from provided header
+                if (context.Request.Headers.ContainsKey("X-MS-CLIENT-PRINCIPAL-ID"))
+                {
+                    // Read headers from Azure
+                    var azureAppServicePrincipalIdHeader = context.Request.Headers["X-MS-CLIENT-PRINCIPAL-ID"][0];
+                    var azureAppServicePrincipalNameHeader = context.Request.Headers["X-MS-CLIENT-PRINCIPAL-NAME"][0];
+
+                    //invoke /.auth/me
+                    var cookieContainer = new CookieContainer();
+                    HttpClientHandler handler = new HttpClientHandler()
+                    {
+                        CookieContainer = cookieContainer
+                    };
+                    string uriString = $"{context.Request.Scheme}://{context.Request.Host}";
+                    foreach (var c in context.Request.Cookies)
+                    {
+                        cookieContainer.Add(new Uri(uriString), new Cookie(c.Key, c.Value));
+                    }
+                    string jsonResult = string.Empty;
+                    using (HttpClient client = new HttpClient(handler))
+                    {
+                        var res = await client.GetAsync($"{uriString}/.auth/me");
+                        jsonResult = await res.Content.ReadAsStringAsync();
+                    }
+
+                    //parse json
+                    var obj = JArray.Parse(jsonResult);
+                    string user_id = obj[0]["user_id"].Value<string>(); //user_id
+
+                    // Create claims id
+                    List<Claim> claims = new List<Claim>();
+                    foreach (var claim in obj[0]["user_claims"])
+                    {
+                        claims.Add(new Claim(claim["typ"].ToString(), claim["val"].ToString()));
+                    }
+
+                    // Set user in current context as claims principal
+                    var identity = new GenericIdentity(azureAppServicePrincipalIdHeader);
+                    identity.AddClaims(claims);
+
+                    // Set current thread user to identity
+                    context.User = new GenericPrincipal(identity, null);
+                };
+
+                await next.Invoke();
+            });
 
             app.UseStaticFiles();
 
