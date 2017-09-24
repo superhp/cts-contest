9a5fb120505babf466f2177e14fc025b38535631
Aurimas Šimkus <saurimas@hotmail.com>
2017-09-24 23:30:22 +0300

    CtsContestBoard project added

---
+++ CtsContestBoard/src/app.js
@@ -0,0 +1,8 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from 'Board.jsx';
+
+ReactDOM.render(
+   <Board />,
+   document.getElementById('Content')
+);
---
+++ CtsContestBoard/webpack.config.js
@@ -0,0 +1,24 @@
+﻿"use strict";
+
+var webpack = require("webpack");
+module.exports = {
+   entry: "./src/app.js",
+   output: {
+      filename: "./wwwroot/bundle.js",
+      publicPath: '/dist/'
+   },
+   resolve: {
+      modules: ["src", "node_modules"]
+   },
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            loader: "babel-loader",
+            query: {
+               presets: ["es2015", "react"]
+            }
+         }
+      ]
+   }
+};
