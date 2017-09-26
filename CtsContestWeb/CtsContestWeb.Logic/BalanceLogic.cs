1e64d3da3b5fdacb4a264f8233793de5502c7edc
Gerda Simoliunaite <gerda.simoliunaite@storebrand.no>
2017-09-26 15:40:14 +0300

    Implemented balance logic

--- CtsContestWeb/CtsContestWeb.Logic/BalanceLogic.cs
+++ CtsContestWeb/CtsContestWeb.Logic/BalanceLogic.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿using CtsContestWeb.Communication;
+using CtsContestWeb.Db;
+using CtsContestWeb.Db.DataAccess;
+using CtsContestWeb.Db.Repository;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CtsContestWeb.Logic
 {
     public class BalanceLogic
     {
         private int _amount;
+        private readonly ISolutionRepository _solRep;
+        private readonly IPurchaseRepository _purRep;
+        public ITask TaskManager { get; }
+        public IPrize PrizeManager { get; }
 
-        public BalanceLogic(/* ... */)
+        public BalanceLogic(ISolutionRepository solRep, ITask taskManager, IPurchaseRepository purRep, IPrize prizeManager)
         {
             _amount = 0;
+            _solRep = solRep;
+            TaskManager = taskManager;
+            _purRep = purRep;
+            PrizeManager = prizeManager;
         }
 
         public int Amount
@@ -16,9 +30,27 @@ namespace CtsContestWeb.Logic
             get { return _amount; }
         }
 
-        public bool IsBalanceEnough()
+        public async Task<bool> IsBalanceEnough(string userEmail, int prizeId)
         {
-            throw new NotImplementedException();
+            var prize = await PrizeManager.GetPrizeById(prizeId);
+            var price = prize.Price;
+            var balance = (await GetTotalEarnedMoney(userEmail)) - GetTotalSpentMoney(userEmail); 
+            return balance >= price;
+        }
+
+        private async Task<int> GetTotalEarnedMoney(string userEmail)
+        {
+            var ids = _solRep.GetTaskIdsByUserEmail(userEmail);
+            var tasks = await System.Threading.Tasks.Task.WhenAll(ids.Select(x => TaskManager.GetTaskById(x)));
+            var sum = tasks.Select(x => x.Value).DefaultIfEmpty(0).Sum();
+            return sum;
+        }
+
+        private int GetTotalSpentMoney(string userEmail)
+        {
+            var purchases = _purRep.GetAllByUserEmail(userEmail);
+            var sum = purchases.Select(x => x.Cost).DefaultIfEmpty(0).Sum();
+            return sum;
         }
     }
 }
