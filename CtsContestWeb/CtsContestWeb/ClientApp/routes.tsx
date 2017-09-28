b95e412ea08e6dd8bec86d326b52c49b3c6f9a47
Kęstutis Kuncė <kestutis.kunce@gmail.com>
2017-09-28 13:02:12 +0300

    remove home page fix

--- CtsContestWeb/CtsContestWeb/ClientApp/routes.tsx
+++ CtsContestWeb/CtsContestWeb/ClientApp/routes.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
-import { Home } from './views/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { Tasks } from './components/Tasks';
