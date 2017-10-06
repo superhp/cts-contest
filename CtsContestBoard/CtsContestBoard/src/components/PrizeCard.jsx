ee82fd1db340dfbf2bcc4ff067936f53f208516d
Gerda Simoliunaite <gerda.simoliunaite@storebrand.no>
2017-10-06 13:24:42 +0300

    Added style to prize board

--- CtsContestBoard/CtsContestBoard/src/components/PrizeCard.jsx
+++ CtsContestBoard/CtsContestBoard/src/components/PrizeCard.jsx
@@ -1,28 +1,36 @@
 ﻿import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Image, Segment, Label } from 'semantic-ui-react';
 
 export default class PrizeCard extends React.Component {
     constructor(props) {
         super(props);
         console.log(props);
     }
+
+    
     render() {
+        let label = label = <h3><p >{this.props.prize.Quantity} left</p></h3>; 
+        if (this.props.prize.Quantity <= 3 && this.props.prize.Quantity > 0) label = <Label color='yellow' ribbon>ONLY {this.props.prize.Quantity} LEFT!</Label>;
+        if (this.props.prize.Quantity === 0) label = <Label color='red' ribbon>SOLD OUT</Label>;
         return (
             <Card>
-                <Image src={this.props.prize.Picture} />
-                <Card.Content>
-                    <Card.Header>
-                        {this.props.prize.Name}
-                    </Card.Header>
-                </Card.Content>
-                <Card.Content extra>
                     
-                    <div className='row'>
-                        <div className='col-xs-6 col-center-block'>
-                            <p>{this.props.prize.Quantity} left</p>
+                    <Image src={this.props.prize.Picture} />
+                    <Card.Content className="cardHeader">
+                    <Card.Header className="headerText">
+                            {this.props.prize.Name}
+                        </Card.Header>
+                    </Card.Content>
+                    <Card.Content>
+                    
+                        <div className='row'>
+                            <div className='col-xs-6 col-center-block'>
+                           
+                            {label}
+                               
+                            </div>
                         </div>
-                    </div>
-                </Card.Content>
+                    </Card.Content>
             </Card>
         )
     }
