24ac33d7da21ab374070f2c24d199dac9be39f04
vstrimaitis <vstrimaitis@gmail.com>
2017-10-28 16:03:56 +0300

    Added TestcaseDto.

---
+++ CtsContestCms/CtsContestCms/Models/TestcaseDto.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace CtsContestCms.Models
+{
+    public class TestcaseDto
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public bool IsSample { get; set; }
+    }
+}
