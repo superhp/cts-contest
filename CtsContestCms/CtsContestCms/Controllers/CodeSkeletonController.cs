5454d12c45e6bebdab8d59ffb44ae97fb4a4a69b
Laurynas <laurynas.simkunas@cognizant.com>
2017-11-14 15:08:22 +0200

    Prizes and skeletons without IP protection

--- CtsContestCms/CtsContestCms/Controllers/CodeSkeletonController.cs
+++ CtsContestCms/CtsContestCms/Controllers/CodeSkeletonController.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using CtsContestCms.Filters;
 using CtsContestCms.Models;
 using Umbraco.Web;
 using Umbraco.Web.WebApi;
 
 namespace CtsContestCms.Controllers
 {
-    [ContestIPHandler]
     public class CodeSkeletonController : UmbracoApiController
     {
         // GET api/codeskeleton/get/{id}
