1ebcd6488d5e58ff5545116425ba3464c050de28
Gerda Simoliunaite <gerda.simoliunaite@storebrand.no>
2018-02-23 16:57:39 +0200

    Created graph for quiz

---
+++ CtsContestWeb/CtsContestWeb/ClientApp/views/Graph.tsx
@@ -0,0 +1,82 @@
+﻿enum NodeType { Q , A }
+
+class GraphNode {
+    id: number;
+    text: string;
+    edges: Edge[];
+    type: NodeType;
+
+    constructor(id: number, text: string, type: NodeType) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.edges = [];
+    }
+}
+
+class Edge {
+    id: number;
+    text: string;
+
+    constructor(id: number, text: string) {
+        this.id = id;
+        this.text = text;
+    }
+}
+
+class Graph {
+    nodes: GraphNode[];
+    curr: GraphNode;
+    constructor() {
+        this.nodes = [];
+    }
+
+    addQuestion(idx: number, question: string) {
+        var node = new GraphNode(idx, question, NodeType.Q);
+        this.nodes.push(node);
+    }
+
+    addAnswer(idx: number, answer: string) {
+        var node = new GraphNode(idx, answer, NodeType.A);
+        this.nodes.push(node);
+    }
+
+    addEdge(from: number, to: number, text: string) {
+        var fromNode = this.nodes.filter(x => x.id === from)[0];
+        fromNode.edges.push({
+            id: to,
+            text: text
+        });
+    }
+
+    setCurr(id: number) {
+        this.curr = this.nodes.filter(x => x.id === id)[0];
+    }
+}
+
+const g = new Graph();
+g.addQuestion(0, 'Do you like new challenges & opportunities?');
+g.addQuestion(1, 'Do you speak any European language?');
+g.addQuestion(2, 'Are you interested in finance or IT?');
+g.addQuestion(3, 'Which sector is more interesting for you?');
+g.addQuestion(4, 'Do you have experience working with IT?');
+g.addQuestion(5, 'Would you like to learn a new language?');
+g.addAnswer(6, 'It\'s not a problem! Check our career page for the future possibilities.');
+g.addAnswer(7, 'Congrats! We have an offer only for you!');
+g.addAnswer(8, 'Great! Check out our career page for IT sector jobs.');
+g.addAnswer(9, 'It\'s not a problem! We have an offer only for you!');
+
+g.addEdge(0, 1, "Yes");
+g.addEdge(1, 2, "Yes");
+g.addEdge(2, 3, "Yes");
+g.addEdge(4, 8, "Yes");
+g.addEdge(5, 3, "Yes");
+g.addEdge(0, 5, "No");
+g.addEdge(5, 6, "No");
+g.addEdge(1, 5, "No");
+g.addEdge(4, 9, "No");
+g.addEdge(3, 4, "IT");
+g.addEdge(3, 7, "Finance");
+
+g.setCurr(0);
+export default g;
--- CtsContestWeb/CtsContestWeb/ClientApp/views/Quiz.tsx
+++ CtsContestWeb/CtsContestWeb/ClientApp/views/Quiz.tsx
@@ -14,23 +14,9 @@ import {
 } from 'semantic-ui-react'
 import { RouteComponentProps } from 'react-router';
 
+import q from './Graph'; 
 
 export class Quiz extends React.Component<RouteComponentProps<{}>, {}> {
-    myMap = {
-        0: 'Do you like new challenges & opportunities?',
-        1: 'Do you speak any European language?',
-        2: 'Are you interested in finance or IT?',
-        3: 'Which sector is more interesting for you?',
-        4: 'Do you have experience working with IT?',
-        5: 'Would you like to learn a new language?',
-        6: 'It\'s not a problem! Check our career page for the future possibilities.',
-        7: 'Congrats! We have an offer only for you!',
-        8: 'Great! Check out our careen page for IT sector jobs.',
-        9: 'Yes',
-        10: 'No',
-        11: 'IT',
-        12: 'Finance'
-    }
     public render() {
         return (
             <div className='cg-prize-page'>
@@ -47,15 +33,14 @@ export class Quiz extends React.Component<RouteComponentProps<{}>, {}> {
                 <Segment style={{ padding: '1em 0em 3em' }} vertical>
                     <Container>
                         <div className='cg-title'>
-                            <h2>{this.myMap[0]}</h2>
+                            <h2>{q.curr.text}</h2>
                         </div>
                         <div className="cg-quiz-button">
-                            <div className='cg-action-item'>
-                                <button className='cg-card-button cyan'>{this.myMap[9]}</button>
-                            </div>
-                            <div className='cg-action-item'>
-                                <button className='cg-card-button cyan'>{this.myMap[10]}</button>
-                            </div>
+                            {q.curr.edges.map(x =>
+                                <div className='cg-action-item'>
+                                    <button className='cg-card-button cyan'>{x.text}</button>
+                                </div>
+                            )}
                         </div>
                     </Container>
                 </Segment>
