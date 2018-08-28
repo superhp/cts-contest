c94119b4877cf2936bb62bf747b4c1d46ab50a30
Aurimas Šimkus <saurimas@hotmail.com>
2018-08-28 12:44:00 +0300

    Menu links uncommented

--- CtsContestWeb/CtsContestWeb/ClientApp/components/Header.tsx
+++ CtsContestWeb/CtsContestWeb/ClientApp/components/Header.tsx
@@ -9,14 +9,14 @@ import { Header as ModalHeader, Image, Modal } from 'semantic-ui-react'
 import { Login } from './Login';
 
 const links = [
-    // {
-    //     routeTo: '/',
-    //     name: 'About'
-    // },
-    // {
-    //     routeTo: '/tasks',
-    //     name: 'Tasks'
-    // },
+	{
+        routeTo: '/',
+        name: 'About'
+    },
+    {
+        routeTo: '/tasks',
+        name: 'Tasks'
+    },
     {
         routeTo: '/',
         name: 'Competition'
@@ -25,18 +25,18 @@ const links = [
         routeTo: '/leaderboard',
         name: 'Leaderboard'
     },
-    // {
-    //     routeTo: '/shop',
-    //     name: 'Shopping booth'
-    // },
-    // {
-    //     routeTo: '/prizes',
-    //     name: 'Day Prize'
-    // },
-    // {
-    //     routeTo: '/quiz',
-    //     name: 'Quiz'
-    // }
+    {
+        routeTo: '/shop',
+        name: 'Shopping booth'
+    },
+    {
+        routeTo: '/prizes',
+        name: 'Day Prize'
+    },
+    {
+        routeTo: '/quiz',
+        name: 'Quiz'
+    }
 ];
 
 export type HeaderState = {
