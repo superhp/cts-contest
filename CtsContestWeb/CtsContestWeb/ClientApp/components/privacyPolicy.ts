8c2aba1aa74aeaa80c7dcae83336f609911880d1
Obolevicius <TAOB01@storebrand.com>
2018-11-06 17:05:21 +0200

    New privacy policy text added

---
+++ CtsContestWeb/CtsContestWeb/ClientApp/components/privacyPolicy.ts
@@ -0,0 +1,58 @@
+export const privacyPolicy = "This Privacy Policy describes how your personal information is collected, used, and shared when you visit cognizantchallenge.lt (the “Site”).\n" +
+"\n" +
+"PERSONAL INFORMATION WE COLLECT\n" +
+"When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as “Device Information”.\n" +
+"\n" +
+"We collect Device Information using the following technologies:\n" +
+"- “Cookies” are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit http://www.allaboutcookies.org.\n" +
+"- “Log files” track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.\n" +
+"- “Web beacons”, “tags”, and “pixels” are electronic files used to record information about how you browse the Site.\n" +
+"\n" +
+"Additionally when you connect to the game through the Site, we collect certain information from you, including your name, e-mail address.\n" +
+"\n" +
+"\n" +
+"When we talk about “Personal Information” in this Privacy Policy, we are talking both about Device Information and Information about you.\n" +
+"\n" +
+"HOW DO WE USE YOUR PERSONAL INFORMATION?\n" +
+"We use this Information to:\n" +
+"- Communicate with you;\n" +
+"- When in line with the preferences you have shared with us, provide you with information or advertising relating to job vacancies, marketing campaigns.\n" +
+"\n" +
+"We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address), and more generally to improve and optimize our Site (for example, by generating analytics about how our customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns).\n" +
+"\n" +
+"SHARING YOUR PERSONAL INFORMATION\n" +
+"We share your Personal Information with third parties to help us use your Personal Information, as described above. For example, we use Google Analytics to help us understand how our visitors use the Site -- you can read more about how Google uses your Personal Information here: https://www.google.com/intl/en/policies/privacy/. You can also opt-out of Google Analytics here: https://tools.google.com/dlpage/gaoptout.\n" +
+"\n" +
+"Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.\n" +
+"\n" +
+"BEHAVIOURAL ADVERTISING\n" +
+"As described above, we use your Personal Information to provide you with targeted advertisements or marketing communications we believe may be of interest to you. For more information about how targeted advertising works, you can visit the Network Advertising Initiative’s (“NAI”) educational page at http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work.\n" +
+"\n" +
+"You can opt out of targeted advertising by using the links below:\n" +
+"- Facebook: https://www.facebook.com/settings/?tab=ads\n" +
+"- Google: https://www.google.com/settings/ads/anonymous\n" +
+"- Bing: https://advertise.bingads.microsoft.com/en-us/resources/policies/personalized-ads\n" +
+"\n" +
+"Additionally, you can opt out of some of these services by visiting the Digital Advertising Alliance’s opt-out portal at: http://optout.aboutads.info/.\n" +
+"\n" +
+"DO NOT TRACK\n" +
+"Please note that we do not alter our Site’s data collection and use practices when we see a Do Not Track signal from your browser.\n" +
+"\n" +
+"YOUR RIGHTS\n" +
+"If you are a European resident, you have the right to access personal information we hold about you and to ask that your personal information be corrected, updated, or deleted. If you would like to exercise this right, please contact us through the contact information below.\n" +
+"\n" +
+"Additionally, if you are a European resident we note that we are processing your information in order to fulfill contracts we might have with you, or otherwise to pursue our legitimate business interests listed above.\n" +
+"\n" +
+"\n" +
+"CHANGES\n" +
+"We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.\n" +
+"\n" +
+"\n" +
+"CONTACT US\n" +
+"For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by e‑mail at hello@cognizant.com or send us letter\n" +
+"\n" +
+"Cognizant Lithuania\n" +
+"\n" +
+"Savanoriu ave. 16\n" +
+"\n" +
+"Vilnius, Lithuania"
