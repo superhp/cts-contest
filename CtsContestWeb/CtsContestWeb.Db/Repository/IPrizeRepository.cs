e3ffad2cce3c1b62ee4b31feca74568fd37921b7
edgar447 <edgar.reis447@gmail.com>
2019-05-28 10:21:20 +0300

    Prize repository

---
+++ CtsContestWeb/CtsContestWeb.Db/Repository/IPrizeRepository.cs
@@ -0,0 +1,14 @@
+﻿using CtsContestWeb.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CtsContestWeb.Db.Repository
+{
+    public interface IPrizeRepository
+    {
+        List<PrizeDto> GetAllWinnablePrizes();
+        List<PrizeDto> GetAllPrizesForPoints();
+        PrizeDto GetPrizeById(int id);
+    }
+}
+
---
+++ CtsContestWeb/CtsContestWeb.Db/Repository/PrizeRepository.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CtsContestWeb.Db.Entities;
+using CtsContestWeb.Dto;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using RestSharp;
+
+namespace CtsContestWeb.Db.Repository
+{
+    public class PrizeRepository : IPrizeRepository
+    {
+        private readonly IPurchaseRepository _purchaseRepository;
+        private readonly IMemoryCache _cache;
+        private readonly ApplicationDbContext _dbContext;
+
+        public PrizeRepository(IPurchaseRepository purchaseRepository, IMemoryCache cache, ApplicationDbContext dbContext)
+        {
+            _purchaseRepository = purchaseRepository;
+            _cache = cache;
+            _dbContext = dbContext;
+        }
+        public List<PrizeDto> GetAllWinnablePrizes()
+        {
+            List<PrizeDto> winnablePrizes;
+     //       if (!_cache.TryGetValue<List<PrizeDto>>("winnablePrizes", out winnablePrizes))
+     {
+         winnablePrizes = GetAllWinnablePrizesFromDb(); //CacheWinnablePrizes();
+     }
+
+            return winnablePrizes;
+        }
+
+        public List<PrizeDto> CacheWinnablePrizes()
+        {
+            var winnablePrizes = GetAllWinnablePrizesFromDb();
+
+            MemoryCacheEntryOptions cacheExpirationOptions = new MemoryCacheEntryOptions();
+            cacheExpirationOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(60);
+            cacheExpirationOptions.Priority = CacheItemPriority.Normal;
+            _cache.Set<List<PrizeDto>>("winnablePrizes", winnablePrizes, cacheExpirationOptions);
+
+            return winnablePrizes;
+        }
+
+        private List<PrizeDto> GetAllWinnablePrizesFromDb()
+        {
+
+            var prizes = _dbContext.Prizes.ToList();
+            var winnablePrizes = prizes.Where(p => !p.Category.Equals("Prize for points")).ToList();
+            var purchases = _purchaseRepository.GetAll().ToList();
+            var winnableDtoPrizes = new List<PrizeDto>();
+
+            foreach (var item in winnablePrizes)
+            {
+                var dtoItem = MapToDto(item);
+                dtoItem.Quantity = purchases.Count(np => np.PrizeId == item.Id);
+                //==========================
+                dtoItem.Quantity = 5;
+                //======================
+                winnableDtoPrizes.Add((dtoItem));
+            }
+            return winnableDtoPrizes;
+        }
+
+        public List<PrizeDto> GetAllPrizesForPoints()
+        {
+            var prizes = _dbContext.Prizes.ToList();
+            var prizesForPoints = prizes.Where(p => p.Category.Equals("Prize for points")).ToList();
+            var purchases = _purchaseRepository.GetAll().ToList();
+            var winnableDtoPrizes = new List<PrizeDto>();
+
+            foreach (var item in prizesForPoints)
+            {
+                var dtoItem = MapToDto(item);
+                dtoItem.Quantity = purchases.Count(np => np.PrizeId == item.Id);
+                //==========================
+                dtoItem.Quantity = 5;
+                //==========================
+                winnableDtoPrizes.Add(dtoItem);
+            }
+            return winnableDtoPrizes;
+        }
+
+        public PrizeDto GetPrizeById(int id)
+        {
+
+            var prize = _dbContext.Prizes.Find(id);
+            if (prize == null)
+            {
+                throw new ArgumentException("No prize with given ID");
+            }
+
+            var purchases = _purchaseRepository.GetAll();
+            var purchasesCount = purchases.Count(p => p.PrizeId == prize.Id);
+            var dtoPrize = MapToDto(prize);
+            dtoPrize.Quantity -= purchasesCount;
+
+            return dtoPrize;
+        }
+
+        private PrizeDto MapToDto(Prize prize)
+        {
+            return  new PrizeDto
+            {
+                Name = prize.Name,
+                Id = prize.Id,
+                Category = prize.Category,
+                Description = prize.Description,
+                Picture = prize.Picture,
+                Price = prize.Price,
+                Quantity = prize.Quantity,
+            };
+        }
+    }
+}
