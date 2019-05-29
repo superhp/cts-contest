1d0b3b60f3dd99c34f0dc21c3b1132370c5ae211
Jurgis <jurgis.kargaudas@gmail.com>
2019-05-29 11:21:04 +0300

    Remove links from infographic in About page

--- CtsContestWeb/CtsContestWeb/ClientApp/views/About.tsx
+++ CtsContestWeb/CtsContestWeb/ClientApp/views/About.tsx
@@ -63,33 +63,27 @@ export class About extends React.Component<RouteComponentProps<{}>, {}> {
                     <Container style={{ paddingTop: '3em' }}>
                         <div className='cg-infographic-container'>
                             <div className='cg-infographic'>
-                                <a href="https://cts-contest.azurewebsites.net/login">
                                 <i className='lock icon cg-infographic-icon'></i>
                                 <div className='cg-infographic-text-container'>
                                     <div className='cg-infographic-title'>Sign in</div>
                                     <div className='cg-infographic-text'>via Facebook or Google</div>
-                                    </div>
-                                 </a>
+                                </div>
                             </div>
                             <div className='cg-infographic'>
-                                <a href="https://cts-contest.azurewebsites.net/tasks">
                                 <i className='laptop icon cg-infographic-icon'></i>
                                 <div className='cg-infographic-text-container'>
                                     <div className='cg-infographic-title'>Solve</div>
                                     <div className='cg-infographic-text'>tasks and earn coins</div>
                                     {/* <div className='cg-infographic-text-small'>individually or in a duel</div>     */}
-                                    </div>
-                                    </a>
+                                </div>
                             </div>
                             <div className='cg-infographic'>
-                                <a href="https://cts-contest.azurewebsites.net/shop">
                                 <i className='shopping cart icon cg-infographic-icon'></i>
                                 <div className='cg-infographic-text-container'>
                                     <div className='cg-infographic-title'>Spend</div>
                                     <div className='cg-infographic-text'>coins you have earned</div>
                                     <div className='cg-infographic-text-small'>buy at booth or save for big prize</div>
-                                    </div>
-                                    </a>
+                                </div>
                             </div>
                         </div>
                     </Container>
